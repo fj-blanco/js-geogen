53c79cc51113a0d44c679fd20fe7101af35b6c42
fj-blanco <fjavib@gmail.com>
2021-01-02 01:03:09 +0100

    1 file modified

--- index.js
+++ index.js
@@ -3,6 +3,21 @@
 
 var municipios_json_link = "https://raw.githubusercontent.com/fj-blanco/js-geogen/master/municipios/municipios_data.json";
 
+/*var mymap = L.map('mapid').setView([40.4169, -3.7035], 6);
+    
+// OSM Baselayer
+//L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZmphdmliIiwiYSI6ImNramVvbGlmZDJtYzAycXJ1NGczdTBkaGgifQ.bU5WrKn-xNm5iv_F20QEmA'
+}).addTo(mymap);
+
+var heat = L.heatLayer([40.4169, -3.7035], {radius:12,blur:25,maxZoom:11}).addTo(mymap);*/
+
 // gedcom_handler:
 const gedcom_file = document.getElementById('gedcom_upload_button');
 gedcom_file.addEventListener("change", function() {
@@ -34,7 +49,8 @@ function json_handler(data, arr_places_names) {
     var arr_places_coordinates = [];
     for (k = 0; k< arr_places_data.length; k++)
           arr_places_coordinates.push([arr_places_data[k].LATITUD_ETRS89, arr_places_data[k].LONGITUD_ETRS89]);
-          console.log([arr_places_data[k].LATITUD_ETRS89, arr_places_data[k].LONGITUD_ETRS89]);
+          //console.log([arr_places_data[k].LATITUD_ETRS89, arr_places_data[k].LONGITUD_ETRS89]);
+    //var heat = L.heatLayer(arr_places_coordinates, {radius:12,blur:25,maxZoom:11}).addTo(mymap);
 }
 // Getting the municipalities json by url:
 function get_municipios_json_parser(handler_callback) {
